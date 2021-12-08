9eaf578e5c092f5b2717c3fee913e7f5a77dae2e
JessGH <jgh@jgh.me.uk>
2021-12-08 15:57:56 +0000

    Rename combination to code for length and consistency with discussions of the game

--- game.py
+++ game.py
@@ -17,16 +17,16 @@ class game_states:
 
 
 class CodeMaker:
-    def __init__(self, combination=None, n_symbols=6, length=4, max_guesses=10):
+    def __init__(self, code=None, n_symbols=6, length=4, max_guesses=10):
         self.symbols = symbols[:n_symbols]
         self.length = length
         self.max_guesses = max_guesses
-        if combination is not None:
-            if len(combination) != self.length:
-                raise ValueError("Combination is the wrong length")
-            for s in combination:
+        if code is not None:
+            if len(code) != self.length:
+                raise ValueError("Code is the wrong length")
+            for s in code:
                 if s not in self.symbols:
-                    raise ValueError("Bad symbol in combination")
-            self.combination = combination
+                    raise ValueError("Bad symbol in code")
+            self.code = code
         else:
-            self.combination = random.choices(self.symbols, k=length)
+            self.code = random.choices(self.symbols, k=length)
